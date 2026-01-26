1001c5a49b7a1e7d00ea9814023839890dcef7e1
Zujajah Batool <batoolzujajah@gmail.com>
2026-01-26 19:57:22 +0500

    Time: 3 ms (30.7%), Space: 10.3 MB (24.95%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int p1=num1.size()-1, p2=num2.size()-1, carry=0;
+        string result="";
+        while(p1>=0 ||p2>=0 ||carry){
+            long sum=0;
+            if(p1>=0){
+                sum += num1[p1] - '0';
+                p1--;
+            }
+            if(p2>=0){
+                sum += num2[p2] - '0';
+                p2--;
+            }
+            sum+=carry;
+            carry=sum/10;
+            result+=to_string(sum%10);
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
