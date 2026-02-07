a879e13d218a196cfa2992dbe537698b31c794d3
Zujajah Batool <batoolzujajah@gmail.com>
2026-02-07 22:14:42 +0500

    Time: 14 ms (6.81%), Space: 25.9 MB (9.43%) - LeetHub

---
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0){
+                result.push_back(nums[i]);
+            }
+            else if(nums[i] > 0){
+                int index = (i + nums[i]) % n;
+                result.push_back(nums[index]);
+            }
+            else{
+                int index = ((i + nums[i]) % n + n) % n;
+                result.push_back(nums[index]);
+            }
+        }
+        return result;
+    }
+};
